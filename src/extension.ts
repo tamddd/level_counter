0d78b4a3c81125ca82b6ae8866d002dc197024d3
tamura daiki <daiki1106tamu@gmail.com>
2021-10-16 10:34:09 +0900

    first commit

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,34 @@
 'use strict';
 import * as vscode from 'vscode';
-import { Extension } from 'typescript';
+import { Extension, transpileModule } from 'typescript';
 import debounce = require('lodash.debounce');
 import * as path from 'path';
+import { exit } from 'process';
 
 const { exec } = require('child_process');
 var player = require('play-sound')();
 const _basePath: string = path.join(__dirname, '..');
-const EXPERIENCE_KEY = 'type_counter11';
-const LEVEL_KEY = 'type_counter12';
-const PREVIOUS_TYPE = 'type_counter13';
+const EXPERIENCE_KEY = 'type_counter20';
+const LEVEL_KEY = 'type_counter21';
+const PREVIOUS_TYPE = 'type_counter22';
 
 //seのpath宣言
 const _saveAudio: string = path.join(_basePath, 'music', 'save.mp3');
 const _levelUpAudio: string = path.join(_basePath, 'music', 'levelUp.mp3');
 
+function getRandomInt(min:number, max:number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  };  
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext, event: vscode.TextDocumentChangeEvent) {
     let activeEditor = vscode.window.activeTextEditor;
+    let actionMode = false;
     //経験値とレベルを表示するウインドウの作成,表示
-    let  _channell: vscode.OutputChannel = vscode.window.createOutputChannel("経験値");
+    let _channell: vscode.OutputChannel = vscode.window.createOutputChannel("経験値");
     _channell.show(true);
     //全体で共通のストレージにKey-Value型で保存
     let exp = context.globalState.get(EXPERIENCE_KEY, 0);
@@ -35,7 +43,81 @@ export function activate(context: vscode.ExtensionContext, event: vscode.TextDoc
     _channell.appendLine(`あなたのレベルは ${level} です`);
     _channell.appendLine(`経験値: ${"*".repeat(experience/10)}`);
 
+    let appearance = getRandomInt(1, 150);
+
+    const disposable = vscode.commands.registerCommand('exp.log', () => {
+        _channell.clear();
+        _channell.appendLine(`次のレベルまで: ${150-experience}`);
+    });
+
+    const escape = vscode.commands.registerCommand('battle.escape', () => {
+        if (actionMode === true){
+        _channell.clear();
+        _channell.appendLine(`敵から逃げました`);
+        actionMode = false;
+        }
+    });
+
+    const monsterAppearance = vscode.commands.registerCommand('monster.appearance', () => {
+        let activeEditor = vscode.window.activeTextEditor;
+        actionMode = true;
+        _channell.clear();
+        _channell.appendLine(`敵があらわれた`);
+        _channell.appendLine(`    人     `);
+        _channell.appendLine(`  ( .. )`);
+
+        let myHp = getRandomInt(20, 35);
+        let monsterHp = getRandomInt(30, 50);
+        _channell.appendLine(`自分の体力: ${myHp}`);
+        _channell.appendLine(`敵の体力: ${monsterHp}`);
+
+        if (actionMode === true){
+            vscode.workspace.onDidChangeTextDocument(event => {
+                if (activeEditor && event.document === activeEditor.document && actionMode === true) {
+                    for (const change of event.contentChanges) {
+                        if (change.text === ''){
+                        if(event.contentChanges[0].rangeLength === 1){
+                            // バックスペースかデリートが押された時
+                            myHp -= 1;
+                        }
+                        }
+                        else{
+                            monsterHp -= 1;
+                        }
+                        _channell.clear();
+                        _channell.appendLine(`    人     `);
+                        _channell.appendLine(`  ( .. )`);
+                        _channell.appendLine(`自分の体力: ${myHp}`);
+                        _channell.appendLine(`敵の体力: ${monsterHp}`);
+
+                        if (myHp <= 0)
+                        {
+                            actionMode = false;
+                            _channell.appendLine(`体力がゼロになりました`);
+                            _channell.appendLine(`経験値を失いました`);
+                            experience -= experience;
+                            break;
+                        }
+
+                        if (monsterHp <= 0)
+                        {
+                            actionMode = false;
+                            _channell.appendLine(`敵を倒した`);
+                            _channell.appendLine(`モンスターを倒しました`);
+                            let monsterExperience = getRandomInt(40, 60);
+                            _channell.appendLine(`${monsterExperience}の経験値を得ました`);
+                            break;
+                        }
+                    }
+                }
+        });
+    }
+        return;
+    });
+
+
     const changeDocumentSubscription = vscode.workspace.onDidChangeTextDocument(e => {
+        if (!actionMode){
         experience ++;
         prev ++;
         context.globalState.update(PREVIOUS_TYPE, prev);
@@ -58,6 +140,7 @@ export function activate(context: vscode.ExtensionContext, event: vscode.TextDoc
         _channell.appendLine(`あなたのレベルは ${level} です`);
         _channell.appendLine(`経験値: ${"*".repeat(experience/10)}`);
         }
+    }
       });
 }
 
